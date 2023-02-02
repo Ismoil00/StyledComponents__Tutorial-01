8fe732bfc1c95e118f448442097463ffff6800a3
Ismoil Sharifov <sharipovismoil01@gmail.com>
2023-02-02 07:52:31 +0500

    init commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledHeader } from "./styles/Header.style";
+
+const Header = () => {
+  return (
+    <StyledHeader bg={"#ebfbff"} cursor={"pointer"}>
+      <h1>Hubble</h1>
+    </StyledHeader>
+  );
+};
+
+export default Header;
